d07cf6415766bda07202143f3ef05d8c0d41cb51
kobyjackcheese <kwrape@icloud.com>
2023-07-11 20:24:53 -0500

    created data structure and models ready for database deploy

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Build
+from .models import Build, SpellType, Spell, Character, WeaponType, Weapon, Talisman, Helmet, Chestplate, Gloves, Leggings
 
 # Register your models here.
 admin.site.register(Build)
+admin.site.register(SpellType)
+admin.site.register(Spell)
+admin.site.register(Character)
+admin.site.register(WeaponType)
+admin.site.register(Weapon)
+admin.site.register(Talisman)
+admin.site.register(Helmet)
+admin.site.register(Chestplate)
+admin.site.register(Gloves)
+admin.site.register(Leggings)
--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class Build(models.Model):
@@ -13,3 +14,139 @@ class Build(models.Model):
 
     class Meta:
         ordering = ['name']
+
+
+class SpellType(models.Model):
+
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Spell(models.Model):
+
+    name = models.CharField(max_length=150)
+    img = models.CharField(max_length=500)
+    SpellType = models.ForeignKey(SpellType, on_delete=models.CASCADE, related_name="spells")
+
+    def __str__(self):
+        return self.name
+    
+class WeaponType(models.Model):
+
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+class Weapon(models.Model):
+
+    name = models.CharField(max_length=150)
+    img = models.CharField(max_length=500)
+    WeaponType = models.ForeignKey(WeaponType, on_delete=models.CASCADE, related_name="spells")
+
+    def __str__(self):
+        return self.name
+    
+class Talisman(models.Model):
+
+    name = models.CharField(default="", max_length=100)
+    img = models.CharField(default="", max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+class Helmet(models.Model):
+
+    name = models.CharField(default="", max_length=100)
+    img = models.CharField(default="", max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+class Chestplate(models.Model):
+
+    name = models.CharField(default="", max_length=100)
+    img = models.CharField(default="", max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+class Gloves(models.Model):
+
+    name = models.CharField(default="", max_length=100)
+    img = models.CharField(default="", max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+class Leggings(models.Model):
+
+    name = models.CharField(default="", max_length=100)
+    img = models.CharField(default="", max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+    
+
+
+
+    # build model i made to test many to many relations that i will use as the build maker for final product
+class Character(models.Model):
+
+    name = models.CharField(max_length=150)
+    img = models.CharField(max_length=500)
+    description = models.CharField(max_length=500)
+    vigStat = models.IntegerField(default=0, validators=[MaxValueValidator(99)])
+    mindStat = models.IntegerField(default=0, validators=[MaxValueValidator(99)])
+    endStat = models.IntegerField(default=0, validators=[MaxValueValidator(99)])
+    strStat = models.IntegerField(default=0, validators=[MaxValueValidator(99)])
+    dexStat = models.IntegerField(default=0, validators=[MaxValueValidator(99)])
+    intStat = models.IntegerField(default=0, validators=[MaxValueValidator(99)])
+    faiStat = models.IntegerField(default=0, validators=[MaxValueValidator(99)])
+    arcStat = models.IntegerField(default=0, validators=[MaxValueValidator(99)])
+    
+    
+    #many to many relationships
+    spells = models.ManyToManyField(Spell)
+    weapons = models.ManyToManyField(Weapon)
+    talismans = models.ManyToManyField(Talisman)
+    helmet = models.ManyToManyField(Helmet)
+    chestplate = models.ManyToManyField(Chestplate)
+    gloves = models.ManyToManyField(Gloves)
+    leggings = models.ManyToManyField(Leggings)
+
+    def __str__(self):
+        return self.name
+
